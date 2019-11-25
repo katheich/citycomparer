98a60d9ea9c9ff05255b83a968252e70e9c02978
Kathrin Eichinger <kathrin801@gmail.com>
2019-11-25 15:48:03 +0000

    home render in app

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
-const Home = (logo) => (
+const Home = (props) => (
   <section className="hero is-fullheight-with-navbar">
-    {console.log(logo)}
     <div className="hero-body">
       <div className="container" id="logo">
 
         <figure className="image is-128x128 has-text-centered" >
-          <img src={logo} alt=""/>
+          <img src={props.logo} alt=""/>
         </figure>
         
         <div className="title">Metropolists</div>
