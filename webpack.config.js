f787124044808e6d3f9087935721a906c2282a67
Kathrin Eichinger <kathrin801@gmail.com>
2019-11-25 14:17:34 +0000

    url loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,10 @@ module.exports = {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
       { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.png$/, loader: 'file-loader' }
+      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.(jpg|png|gif)/, use: [{
+        loader: 'url-loader', options: { limit: 5000 }
+      }] }
     ]
   },
   devServer: {
