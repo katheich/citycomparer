2f67d4228d3e222ed227c9daa3604fd1a93e065e
Kathrin Eichinger <kathrin801@gmail.com>
2019-11-25 15:21:37 +0000

    images import

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve('dist'),
-    publicPath: '/metropolists'
+    publicPath: '/metropolists/'
   },
   module: {
     rules: [
